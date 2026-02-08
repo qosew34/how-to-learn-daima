541074ad6bb03c0dbadbedf94bc54e2763840b73
Nitrev <qlsuce@hotmail.com>
2026-02-08 23:15:52 +0800

    写了一点

--- smalltoys/noguess_minesweeper_killer/tradition.cpp
+++ smalltoys/noguess_minesweeper_killer/tradition.cpp
@@ -20,6 +20,101 @@ void tradition_killer(std :: vector<std :: vector<char>>& board,int n){
             if(temp!='/') is_empty[i][j] = false;
         }
     }
-
-
+    int total = -1;
+    std :: cout<<"如果雷剩余数量已知，输入雷数量，否则输入-1";
+    std :: cin>>total;
+    std :: cout<<"你先别急";
+    //没什么好的思路，穷举？
+    //回溯
+    if(total = -1){//启动剪枝
+        checker_cut(board,n,0,0,is_empty,is_mine);
+    }
+    else if(total!=-1){
+        //明天再写
+    }
+}
+void checker_cut(std :: vector<std :: vector<char>> board,int n,int x,int y,std :: vector<std ::vector<bool>>&is_empty,std :: vector<std ::vector<bool>>&is_mine){
+    //这个点是雷的可能，然后传给下一个checker,如果true传回ismine和isempty
+    //非/则过
+    if(board[x][y]!='/'){
+        if(x==n-1&&y==n-1){
+            for(int i = 0 ; i < n ; i++){
+                    for(int j = 0 ; j < n ; j++){
+                        if(board[i][j]=='/'||board[i][j]=='?') is_empty[i][j] = is_empty[i][j]&true;
+                        else is_empty[i][j] = false;
+                        if(board[i][j]=='m') is_mine[i][j] = is_mine[i][j]&true;
+                        else is_mine[i][j] = false;
+                    }
+            }
+        }
+        if(x!=n-1) checker_cut(board,n,x+1,y,is_empty,is_mine);
+        else checker_cut(board,n,0,y+1,is_empty,is_mine);
+    }
+    else{
+        std :: vector<int> xs = {-1,-1,-1,0,0,1,1,1};
+        std :: vector<int> ys = {1,0,-1,1,-1,1,0,-1};
+        //先判定周围有没有数字，非雷可不可能，可以加入剪枝
+        bool can_be_mine = true;
+        bool can_be_empty = true;
+        bool has_number = false;
+        for(int i = 0 ; i < 8 ; i++){
+            if(x+xs[i]<0||x+xs[i]>n-1||y+ys[i]<0||y+ys[i]>n-1) continue;
+            char temp = board[x+xs[i]][y+ys[i]];
+            int x1 = x+xs[i],y1 = y+ys[i];//是数字的点
+            if(temp>='0'&&temp<='9'){
+                has_number = true;
+                int may_be_mine = 0;
+                int must_mine = 0;
+                for(int j = 0 ; j < 8 ; j++){//数m和/的数量
+                    if(x1+xs[j]<0||x1+xs[j]>n-1||y1+ys[j]<0||y1+ys[j]>n-1) continue;
+                    if(x1+xs[j]==x&&y1+ys[j]==y) continue;//不数本格
+                    char a = board[x1+xs[j]][y1+ys[j]];
+                    if(a=='/') may_be_mine++;
+                    if(a=='m') must_mine++;
+                }
+                if(temp-'0'-may_be_mine-must_mine==1){
+                    can_be_empty = false;
+                }
+                else if(temp - must_mine - '0'==0){
+                    can_be_mine = false;
+                }
+            }
+        }
+        if(x==n-1&&y==n-1){
+            if(can_be_empty){
+                board[x][y] = '?';
+                for(int i = 0 ; i < n ; i++){
+                    for(int j = 0 ; j < n ; j++){
+                        if(board[i][j]=='/'||board[i][j]=='?') is_empty[i][j] = is_empty[i][j]&true;
+                        else is_empty[i][j] = false;
+                        if(board[i][j]=='m') is_mine[i][j] = is_mine[i][j]&true;
+                        else is_mine[i][j] = false;
+                    }
+                }
+            }
+            if(can_be_mine){
+                board[x][y] = 'm';
+                for(int i = 0 ; i < n ; i++){
+                    for(int j = 0 ; j < n ; j++){
+                        if(board[i][j]=='/'||board[i][j]=='?') is_empty[i][j] = is_empty[i][j]&true;
+                        else is_empty[i][j] = false;
+                        if(board[i][j]=='m') is_mine[i][j] = is_mine[i][j]&true;
+                        else is_mine[i][j] = false;
+                    }
+                }
+            }
+        }
+        if(!has_number) checker_cut(board,n,x+1,y,is_empty,is_mine);
+        if(can_be_empty){
+            board[x][y] = '?';
+            if(x!=n-1) checker_cut(board,n,x+1,y,is_empty,is_mine);
+            else checker_cut(board,n,0,y+1,is_empty,is_mine);
+        }
+        if(can_be_mine){
+            board[x][y] = 'm';
+            if(x!=n-1) checker_cut(board,n,x+1,y,is_empty,is_mine);
+            else checker_cut(board,n,0,y+1,is_empty,is_mine);
+        }
+        board.shrink_to_fit();//释放内存，也许会有用？
+    }
 }
